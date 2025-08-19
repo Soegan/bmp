502f0a9b98b122a59cb1e521650c35becfbff258
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2025-08-19 17:50:33 +0200

    finished rotate functions

--- main.c
+++ main.c
@@ -24,42 +24,29 @@ int main () {
     fclose(f);
 
     f = fopen("./adventurer2.bmp", "wb");
-
     struct bmp_image* image2 = flip_horizontally(image);
-
     bool written2 = write_bmp(f, image2);
-
     if (written2);
-
     fclose(f);
 
     f = fopen("./adventurer3.bmp", "wb");
-
     struct bmp_image* image3 = flip_vertically(image);
-
     bool written3 = write_bmp(f, image3);
-
     if (written3);
-
     fclose(f);
 
     f = fopen("./adventurer4.bmp", "wb");
-
     struct bmp_image* image4 = rotate_left(image);
-
     bool written4 = write_bmp(f, image4);
-
     if (written4);
-
     fclose(f);
 
     f = fopen("./adventurer5.bmp", "wb");
-
     struct bmp_image* image5 = rotate_right(image);
-
     bool written5 = write_bmp(f, image5);
-
     if (written5);
+    fclose(f);
+
 
     free_bmp_image(image);
 
